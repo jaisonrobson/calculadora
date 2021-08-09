a0fd471ee6b59bd22baea0b21275bebc4e7d4a05
jaisonrobson <xd_jrg@hotmail.com>
2021-08-09 12:37:44 -0300

    #47 - componente button #02

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -7,30 +7,45 @@ import Display from '../components/Display'
 
 export default class Calculator extends Component {
 
+    clearMemory() {
+        console.log('limpar')
+    }
+
+    setOperation(operation) {
+        console.log(operation)
+    }
+
+    addDigit(n) {
+        console.log(n)
+    }
+
     render() {
+        const addDigit = n => this.addDigit(n)
+        const setOperation = op => this.setOperation(op)
+
         return (
             <Fragment>
                 <h1>Calculadora</h1>
 
                 <div className="calculator">
                     <Display value={100} />
-                    <Button label="AC" />
-                    <Button label="/" />
-                    <Button label="7" />
-                    <Button label="8" />
-                    <Button label="9" />
-                    <Button label="*" />
-                    <Button label="4" />
-                    <Button label="5" />
-                    <Button label="6" />
-                    <Button label="-" />
-                    <Button label="1" />
-                    <Button label="2" />
-                    <Button label="3" />
-                    <Button label="+" />
-                    <Button label="0" />
-                    <Button label="." />
-                    <Button label="=" />
+                    <Button label="AC" onClick={() => this.clearMemory()} triple />
+                    <Button label="/" onClick={setOperation} operation />
+                    <Button label="7" onClick={addDigit} />
+                    <Button label="8" onClick={addDigit} />
+                    <Button label="9" onClick={addDigit} />
+                    <Button label="*" onClick={setOperation} operation />
+                    <Button label="4" onClick={addDigit} />
+                    <Button label="5" onClick={addDigit} />
+                    <Button label="6" onClick={addDigit} />
+                    <Button label="-" onClick={setOperation} operation />
+                    <Button label="1" onClick={addDigit} />
+                    <Button label="2" onClick={addDigit} />
+                    <Button label="3" onClick={addDigit} />
+                    <Button label="+" onClick={setOperation} operation />
+                    <Button label="0" onClick={addDigit} double />
+                    <Button label="." onClick={addDigit} />
+                    <Button label="=" onClick={setOperation} operation />
                 </div>
             </Fragment>
         )
