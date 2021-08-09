b41ca30a3637293ce718bf2e53e311c34c74061d
jaisonrobson <xd_jrg@hotmail.com>
2021-08-09 12:53:32 -0300

    #48 - Logica da calculadora parte 1

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -5,10 +5,20 @@ import './Calculator.css'
 import Button from '../components/Button'
 import Display from '../components/Display'
 
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0,
+}
+
 export default class Calculator extends Component {
 
+    state = { ...initialState }
+
     clearMemory() {
-        console.log('limpar')
+        this.setState({ ...initialState })
     }
 
     setOperation(operation) {
@@ -16,7 +26,23 @@ export default class Calculator extends Component {
     }
 
     addDigit(n) {
-        console.log(n)
+        if (n === '.' && this.state.displayValue.includes('.')) return
+
+        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+        const displayValue = currentValue + n
+
+        this.setState({ displayValue, clearDisplay: false })
+
+        if (n !== '.') {
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[i] = newValue
+
+            this.setState({ values })
+            console.log(values)
+        }
     }
 
     render() {
@@ -28,7 +54,7 @@ export default class Calculator extends Component {
                 <h1>Calculadora</h1>
 
                 <div className="calculator">
-                    <Display value={100} />
+                    <Display value={this.state.displayValue} />
                     <Button label="AC" onClick={() => this.clearMemory()} triple />
                     <Button label="/" onClick={setOperation} operation />
                     <Button label="7" onClick={addDigit} />
